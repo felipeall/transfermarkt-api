719b0877b5626802df0a98928cfaf05663895d05
Felipe Allegretti <felipeallh@gmail.com>
2023-02-16 12:10:56 -0300

    Update app index

--- app/main.py
+++ app/main.py
@@ -1,17 +1,24 @@
 from fastapi import FastAPI
-from services.players import TransfermarktPlayers
-from services.search import TransfermarktPlayerSearch
+from starlette.responses import RedirectResponse
+
+from app.services.players import TransfermarktPlayers
+from app.services.search import TransfermarktPlayerSearch
 
 app = FastAPI()
 
 
-@app.get("/players/{player_id}/{player_code}")
-def get_player_info(player_id: str, player_code: str):
-    tfmkt = TransfermarktPlayers(player_id=player_id, player_code=player_code)
-    return tfmkt.get_player_info()
+@app.get("/", include_in_schema=False)
+async def docs_redirect():
+    return RedirectResponse(url='/docs')
 
 
-@app.get("/players/{query}")
+@app.get("/players/{query}", tags=['Players'])
 def search_players(query: str):
     tfmkt = TransfermarktPlayerSearch(query=query)
     return tfmkt.search_player()
+
+
+@app.get("/players/{player_id}/{player_code}", tags=['Players'])
+def get_player_info(player_id: str, player_code: str):
+    tfmkt = TransfermarktPlayers(player_id=player_id, player_code=player_code)
+    return tfmkt.get_player_info()
