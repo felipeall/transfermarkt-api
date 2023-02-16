607e5844d35904095741a7737195567995143304
Felipe Allegretti <felipeallh@gmail.com>
2023-02-16 12:52:48 -0300

    Add player nationality to Player Search service

--- app/services/search.py
+++ app/services/search.py
@@ -24,15 +24,18 @@ class TransfermarktPlayerSearch(TransfermarktSearch):
         if not result_players:
             return None
 
+        result_nationalities: ElementTree = result_players[0].xpath(".//td[img[@class='flaggenrahmen']]")
+
         players_names: list = result_players[0].xpath(".//td[@class='hauptlink']//a//@title")
         players_urls: list = result_players[0].xpath(".//td[@class='hauptlink']//a//@href")
         players_clubs: list = result_players[0].xpath(".//img[@class='tiny_wappen']//@title")
+        player_nationalities: list = [nationality.xpath(".//img//@title") for nationality in result_nationalities]
         players_ids: list = [url.split("/")[-1] for url in players_urls]
         players_codes: list = [url.split("/")[1] for url in players_urls]
 
         return [
-            {"id": idx, "code": code, "name": name, "url": url, "current_club": club}
-            for idx, code, name, url, club in zip(
-                players_ids, players_codes, players_names, players_urls, players_clubs
+            {"id": idx, "code": code, "name": name, "nationality": nationality, "url": url, "current_club": club}
+            for idx, code, name, nationality, url, club in zip(
+                players_ids, players_codes, players_names, player_nationalities, players_urls, players_clubs
             )
         ]
