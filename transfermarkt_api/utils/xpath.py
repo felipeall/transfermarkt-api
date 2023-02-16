7617ea39c3143a56e5ae29eb781166cf94e34315
Felipe Allegretti <felipeallh@gmail.com>
2023-02-15 22:23:35 -0300

    Add XPath locators

---
+++ transfermarkt_api/utils/xpath.py
@@ -0,0 +1,37 @@
+class Players(object):
+    class Header(object):
+        PLAYER_NAME: str = "//h1[@class='data-header__headline-wrapper']//text()"
+        PLAYER_IMAGE_URL: str = "//div[@id='fotoauswahlOeffnen']//img//@src"
+        SHIRT_NUMBER: str = "//span[@class='data-header__shirt-number']//text()"
+        CURRENT_CLUB_NAME: str = "//span[@class='data-header__club']//a//text()"
+        CURRENT_CLUB_URL: str = "//span[@class='data-header__club']//a//@href"
+        CURRENT_CLUB_JOINED: str = "//span[text()='Joined: ']//span//text()"
+        CURRENT_CLUB_CONTRACT_EXPIRES: str = "//span[text()='Contract expires: ']//span//text()"
+        CURRENT_CLUB_CONTRACT_OPTION: str = "//span[contains(text(),'Contract option:')]//following::span[1]//text()"
+
+    class Profile(object):
+        PLAYER_ID: str = "//div[@data-action='profil']//@data-id"
+        PLAYER_URL: str = "//a[@class='tm-subnav-item megamenu']//@href"
+        CURRENT_CLUB_ID: str = "//tm-quick-select-bar//@default-club"
+
+    class Data(object):
+        NAME_IN_HOME_COUNTRY: str = "//span[text()='Name in home country:']//following::span[1]//text()"
+        FULL_NAME: str = "//span[text()='Full name:']//following::span[1]//text()"
+        DATE_OF_BIRTH: str = "//span[text()='Date of birth:']//following::span[1]//a//text()"
+        PLACE_OF_BIRTH_CITY: str = "//span[text()='Place of birth:']//following::span[1]//span//text()"
+        PLACE_OF_BIRTH_COUNTRY: str = "//span[text()='Place of birth:']//following::span[1]//span//img//@title"
+        AGE: str = "//span[text()='Age:']//following::span[1]//text()"
+        HEIGHT: str = "//span[text()='Height:']//following::span[1]//text()"
+        CITIZENSHIP: str = "//span[text()='Citizenship:']//following::span[1]//text()"
+        POSITION: str = "//span[text()='Position:']//following::span[1]//text()"
+        POSITION_MAIN: str = "//dt[contains(text(),'Main position:')]//following::dd[1]//text()"
+        POSITION_OTHER: str = "//dt[contains(text(),'Other position:')]//following::dd//text()"
+        FOOT: str = "//span[text()='Foot:']//following::span[1]//text()"
+        MARKET_VALUE_CURRENT: str = "//div[@class='tm-player-market-value-development__current-value']//text()"
+        MARKET_VALUE_HIGHEST: str = "//div[@class='tm-player-market-value-development__max-value']//text()"
+        PLAYER_AGENT_NAME: str = "//span[text()='Player agent:']//following::span[1]//a//text()"
+        PLAYER_AGENT_URL: str = "//span[text()='Player agent:']//following::span[1]//a//text()"
+        CURRENT_CLUB_NAME: str = "//span[contains(text(),'Current club:')]//following::span[1]//a[2]//text()"
+        CURRENT_CLUB_URL: str = "//span[contains(text(),'Current club:')]//following::span[1]//a[2]//@href"
+        OUTFITTER: str = "//span[contains(text(),'Outfitter:')]//following::span[1]//text()"
+        SOCIAL_MEDIA: str = "//div[@class='socialmedia-icons']//@href"
