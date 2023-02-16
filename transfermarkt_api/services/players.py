f56575f93378740330b143581d246f58e9c1d49b
Felipe Allegretti <felipeallh@gmail.com>
2023-02-15 22:24:55 -0300

    Fix relative imports

--- transfermarkt_api/services/players.py
+++ transfermarkt_api/services/players.py
@@ -1,8 +1,8 @@
 from dataclasses import dataclass, field
 from xml.etree import ElementTree
 
-from transfermarkt_scrapper.utils.utils import clean_dict, request_player_page
-from transfermarkt_scrapper.utils.xpath import Players
+from transfermarkt_api.utils.utils import clean_dict, request_player_page
+from transfermarkt_api.utils.xpath import Players
 
 
 @dataclass
