235e45e845839746968976a4510eb436ea37fc73
Felipe Allegretti <felipeallh@gmail.com>
2023-02-21 12:30:53 -0300

    Implement Competition Clubs endpoint (#14)
    * Add Competition Clubs XPaths
    
    * Add Competition Clubs service
    
    * Implement Competition Clubs endpoint

--- app/api/api.py
+++ app/api/api.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import clubs, players
+from app.api.endpoints import clubs, competitions, players
 
 api_router = APIRouter()
+api_router.include_router(competitions.router, prefix="/competitions", tags=["competitions"])
 api_router.include_router(clubs.router, prefix="/clubs", tags=["clubs"])
 api_router.include_router(players.router, prefix="/players", tags=["players"])
