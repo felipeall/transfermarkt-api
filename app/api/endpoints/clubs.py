168de519bccd6aeab778613ff24ba09d9b70e449
Felipe Allegretti <felipeallh@gmail.com>
2023-03-31 12:05:44 -0300

    Rename API param to Season ID (#17)

--- app/api/endpoints/clubs.py
+++ app/api/endpoints/clubs.py
@@ -24,7 +24,7 @@ def get_club_profile(club_id: str):
 
 
 @router.get("/{club_id}/players")
-def get_club_players(club_id: str, season_year: Optional[str] = None):
-    tfmkt = TransfermarktClubPlayers(club_id=club_id, season_id=season_year)
+def get_club_players(club_id: str, season_id: Optional[str] = None):
+    tfmkt = TransfermarktClubPlayers(club_id=club_id, season_id=season_id)
     club_players = tfmkt.get_club_players()
     return club_players
