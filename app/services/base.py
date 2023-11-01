8eaebbd8f64c4645f65beeb35ad8743cbda28196
Felipe Allegretti <felipeallh@gmail.com>
2023-11-01 17:48:16 -0300

    Create docstrings

--- app/services/base.py
+++ app/services/base.py
@@ -14,11 +14,35 @@ from app.utils.xpath import Commons
 
 @dataclass
 class TransfermarktBase:
+    """
+    Base class for making HTTP requests to Transfermarkt and extracting data from the web pages.
+
+    Args:
+        URL (str): The URL for the web page to be fetched.
+    Attributes:
+        page (ElementTree): The parsed web page content.
+        response (dict): A dictionary to store the response data.
+    """
+
     URL: str
     page: ElementTree = field(default_factory=lambda: None, init=False)
     response: dict = field(default_factory=lambda: {}, init=False)
 
     def make_request(self, url: Optional[str] = None) -> Response:
+        """
+        Make an HTTP GET request to the specified URL.
+
+        Args:
+            url (str, optional): The URL to make the request to. If not provided, the class's URL
+                attribute will be used.
+
+        Returns:
+            Response: An HTTP Response object containing the server's response to the request.
+
+        Raises:
+            HTTPException: If there are too many redirects, or if the server returns a client or
+                server error status code.
+        """
         url = self.URL if not url else url
         try:
             response: Response = requests.get(
@@ -47,22 +71,73 @@ class TransfermarktBase:
         return response
 
     def request_url_bsoup(self) -> BeautifulSoup:
+        """
+        Fetch the web page content and parse it using BeautifulSoup.
+
+        Returns:
+            BeautifulSoup: A BeautifulSoup object representing the parsed web page content.
+
+        Raises:
+            HTTPException: If there are too many redirects, or if the server returns a client or
+                server error status code.
+        """
         response: Response = self.make_request()
         return BeautifulSoup(markup=response.content, features="html.parser")
 
     @staticmethod
     def convert_bsoup_to_page(bsoup: BeautifulSoup) -> ElementTree:
+        """
+        Convert a BeautifulSoup object to an ElementTree.
+
+        Args:
+            bsoup (BeautifulSoup): The BeautifulSoup object representing the parsed web page content.
+
+        Returns:
+            ElementTree: An ElementTree representing the parsed web page content for further processing.
+        """
         return etree.HTML(str(bsoup))
 
     def request_url_page(self) -> ElementTree:
+        """
+        Fetch the web page content, parse it using BeautifulSoup, and convert it to an ElementTree.
+
+        Returns:
+            ElementTree: An ElementTree representing the parsed web page content for further
+                processing.
+
+        Raises:
+            HTTPException: If there are too many redirects, or if the server returns a client or
+                server error status code.
+        """
         bsoup: BeautifulSoup = self.request_url_bsoup()
         return self.convert_bsoup_to_page(bsoup=bsoup)
 
     def raise_exception_if_not_found(self, xpath: str):
+        """
+        Raise an exception if the specified XPath does not yield any results on the web page.
+
+        Args:
+            xpath (str): The XPath expression to query elements on the page.
+
+        Raises:
+            HTTPException: If the specified XPath query does not yield any results, indicating an invalid request.
+        """
         if not self.get_text_by_xpath(xpath):
             raise HTTPException(status_code=404, detail=f"Invalid request (url: {self.URL})")
 
     def get_list_by_xpath(self, xpath: str, remove_empty: Optional[bool] = True) -> Optional[list]:
+        """
+        Extract a list of elements from the web page using the specified XPath expression.
+
+        Args:
+            xpath (str): The XPath expression to query elements on the page.
+            remove_empty (bool, optional): If True, remove empty or whitespace-only elements from
+                the list. Default is True.
+
+        Returns:
+            Optional[list]: A list of elements extracted from the web page based on the XPath query.
+                If remove_empty is True, empty or whitespace-only elements are filtered out.
+        """
         elements: list = self.page.xpath(xpath)
         if remove_empty:
             elements_valid: list = [trim(e) for e in elements if trim(e)]
@@ -79,6 +154,25 @@ class TransfermarktBase:
         iloc_to: Optional[int] = None,
         join_str: Optional[str] = None,
     ) -> Optional[str]:
+        """
+        Extract text content from the web page using the specified XPath expression.
+
+        Args:
+            xpath (str): The XPath expression to query elements on the page.
+            pos (int, optional): Index of the element to extract if multiple elements match the
+                XPath. Default is 0.
+            iloc (int, optional): Extract a single element by index, used as an alternative to 'pos'.
+            iloc_from (int, optional): Extract a range of elements starting from the specified
+                index (inclusive).
+            iloc_to (int, optional): Extract a range of elements up to the specified
+                index (exclusive).
+            join_str (str, optional): If provided, join multiple text elements into a single string
+                using this separator.
+
+        Returns:
+            Optional[str]: The extracted text content from the web page based on the XPath query and
+                optional parameters. If no matching element is found, None is returned.
+        """
         element = self.page.xpath(xpath)
 
         if not element:
@@ -108,6 +202,15 @@ class TransfermarktBase:
             return None
 
     def get_search_last_page_number(self, xpath_base: str) -> int:
+        """
+        Retrieve the last page number for search results based on the provided base XPath.
+
+        Args:
+            xpath_base (str): The base XPath for extracting page number information.
+
+        Returns:
+            int: The last page number for search results. Returns 1 if no page numbers are found.
+        """
         url_page_number_last = self.get_text_by_xpath(xpath_base + Commons.Search.PAGE_NUMBER_LAST)
         url_page_number_active = self.get_text_by_xpath(xpath_base + Commons.Search.PAGE_NUMBER_ACTIVE)
 
