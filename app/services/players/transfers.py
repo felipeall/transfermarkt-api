00433068cc351496385fc18c4ef979bd65fda133
Felipe Allegretti <felipeallh@gmail.com>
2023-11-08 22:15:23 -0300

    Parse player transfer history from Tfmkt endpoint

--- app/services/players/transfers.py
+++ app/services/players/transfers.py
@@ -22,58 +22,45 @@ class TransfermarktPlayerTransfers(TransfermarktBase):
 
     player_id: str = None
     URL: str = "https://www.transfermarkt.com/-/transfers/spieler/{player_id}"
+    URL_TRANSFERS: str = "https://www.transfermarkt.com/ceapi/transferHistory/list/{player_id}"
 
     def __post_init__(self) -> None:
         """Initialize the TransfermarktPlayerTransfers class."""
         self.URL = self.URL.format(player_id=self.player_id)
         self.page = self.request_url_page()
         self.raise_exception_if_not_found(xpath=Players.Profile.NAME)
+        self.transfer_history = self.make_request(url=self.URL_TRANSFERS.format(player_id=self.player_id))
 
-    def __parse_player_transfers_history(self) -> list:
+    def __parse_player_transfer_history(self) -> list:
         """
-        Parse and retrieve the transfer history of the specified player from Transfermarkt.
+        Parse and retrieve the transfer history of the specified player from Transfermarkt,
+        including the unique identifier of each transfer, source club information (ID and name),
+        destination club information (ID and name), transfer date, upcoming status, season, market
+        value at the time of transfer, and transfer fee.
 
         Returns:
-            list: A list of dictionaries containing details of player transfers, including season, date, old club,
-                  new club, market value, and transfer fee.
+            list: A list of dictionaries, each containing details of the player's transfer history,
         """
-        urls = self.get_list_by_xpath(Players.Transfers.TRANSFERS_URLS)
-        seasons = self.get_list_by_xpath(Players.Transfers.SEASONS)
-        dates = self.get_list_by_xpath(Players.Transfers.DATES)
-        old_clubs_urls = self.get_list_by_xpath(Players.Transfers.OLD_CLUBS_URLS)
-        old_clubs_names = self.get_list_by_xpath(Players.Transfers.OLD_CLUBS_NAMES)
-        new_clubs_urls = self.get_list_by_xpath(Players.Transfers.NEW_CLUBS_URLS)
-        new_clubs_names = self.get_list_by_xpath(Players.Transfers.NEW_CLUBS_NAMES)
-        market_values = self.get_list_by_xpath(Players.Transfers.MARKET_VALUES)
-        fees = self.get_list_by_xpath(Players.Transfers.FEES)
-
-        ids = [extract_from_url(url, "transfer_id") for url in urls]
-        old_clubs_ids = [extract_from_url(url) for url in old_clubs_urls]
-        new_clubs_ids = [extract_from_url(url) for url in new_clubs_urls]
+        transfers = self.transfer_history.json().get("transfers")
 
         return [
             {
-                "id": idx,
-                "seasonID": season,
-                "date": date,
-                "oldClubID": from_club_id,
-                "oldClubName": from_club_name,
-                "newClubID": to_club_id,
-                "newClubName": to_club_name,
-                "marketValue": market_value,
-                "fee": fee,
+                "id": extract_from_url(transfer["url"], "transfer_id"),
+                "from": {
+                    "clubID": extract_from_url(transfer["from"]["href"]),
+                    "clubName": transfer["from"]["clubName"],
+                },
+                "to": {
+                    "clubID": extract_from_url(transfer["to"]["href"]),
+                    "clubName": transfer["to"]["clubName"],
+                },
+                "date": transfer["date"],
+                "upcoming": transfer["upcoming"],
+                "season": transfer["season"],
+                "marketValue": transfer["marketValue"],
+                "fee": transfer["fee"],
             }
-            for idx, season, date, from_club_id, from_club_name, to_club_id, to_club_name, market_value, fee in zip(
-                ids,
-                seasons,
-                dates,
-                old_clubs_ids,
-                old_clubs_names,
-                new_clubs_ids,
-                new_clubs_names,
-                market_values,
-                fees,
-            )
+            for transfer in transfers
         ]
 
     def get_player_transfers(self) -> dict:
@@ -85,7 +72,7 @@ class TransfermarktPlayerTransfers(TransfermarktBase):
                   and the timestamp of when the data was last updated.
         """
         self.response["id"] = self.player_id
-        self.response["transfers"] = self.__parse_player_transfers_history()
+        self.response["transfers"] = self.__parse_player_transfer_history()
         self.response["youthClubs"] = safe_split(self.get_text_by_xpath(Players.Transfers.YOUTH_CLUBS), ",")
         self.response["updatedAt"] = datetime.now()
 
