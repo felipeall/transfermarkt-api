206962a3eddf311fdb091f721cd01db568c98a7d
Felipe Allegretti <felipeallh@gmail.com>
2023-12-14 00:16:14 -0300

    Add docstrings

--- app/services/players/injuries.py
+++ app/services/players/injuries.py
@@ -10,16 +10,37 @@ from app.utils.xpath import Players
 
 @dataclass
 class TransfermarktPlayerInjuries(TransfermarktBase):
+    """
+    Represents a service for retrieving and parsing the injury history of a football player on Transfermarkt.
+
+    Args:
+        player_id (str): The unique identifier of the player.
+        page_number (int): The page number of the player's injury history.
+
+    Attributes:
+        URL (str): The URL to fetch the player's injury history data.
+    """
+
     player_id: str = None
     URL: str = "https://www.transfermarkt.com/player/verletzungen/spieler/{player_id}/plus/1/page/{page_number}"
     page_number: int = 1
 
     def __post_init__(self):
+        """Initialize the TransfermarktPlayerInjuries class."""
         self.URL = self.URL.format(player_id=self.player_id, page_number=self.page_number)
         self.page = self.request_url_page()
         self.raise_exception_if_not_found(xpath=Players.Profile.URL)
 
     def __parse_player_injuries(self) -> Optional[List[dict]]:
+        """
+        Parse the injury history of a football player from the retrieved data.
+
+        Returns:
+            list: A list of dictionaries, where each dictionary represents an injury in the
+                player's injury history. Each dictionary contains keys 'season', 'injury', 'from',
+                'until', 'days', 'gamesMissed', and 'gamesMissedClubs' with their respective values.
+
+        """
         injuries: ElementTree = self.page.xpath(Players.Injuries.RESULTS)
         player_injuries = []
 
@@ -48,6 +69,14 @@ class TransfermarktPlayerInjuries(TransfermarktBase):
         return player_injuries
 
     def get_player_injuries(self) -> dict:
+        """
+        Retrieve and parse the injury history of a football player.
+
+        Returns:
+            dict: A dictionary containing the player's unique identifier, current page number,
+                last page number, and injury history.
+
+        """
         self.response["id"] = self.player_id
         self.response["pageNumber"] = self.page_number
         self.response["lastPageNumber"] = self.get_last_page_number()
