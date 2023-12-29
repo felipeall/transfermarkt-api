df4d06698adf3fbcfb4d93d105cc86fdf89a2b0a
Felipe Allegretti <felipeallh@gmail.com>
2023-12-29 20:03:42 -0300

    Implement rate limiting

--- app/main.py
+++ app/main.py
@@ -1,10 +1,23 @@
 import uvicorn
 from fastapi import FastAPI
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
+from slowapi.middleware import SlowAPIMiddleware
+from slowapi.util import get_remote_address
 from starlette.responses import RedirectResponse
 
 from app.api.api import api_router
+from app.settings import settings
 
+limiter = Limiter(
+    key_func=get_remote_address,
+    default_limits=[settings.RATE_LIMITING_FREQUENCY],
+    enabled=settings.RATE_LIMITING_ENABLE,
+)
 app = FastAPI(title="Transfermarkt API")
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+app.add_middleware(SlowAPIMiddleware)
 app.include_router(api_router)
 
 
