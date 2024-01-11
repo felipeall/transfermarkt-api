0d7bf4850ea34896da969590f034b06edaa91d2f
Felipe Allegretti <felipeallh@gmail.com>
2024-01-11 15:20:31 -0300

    Refactor Players Search parsing (#55)
    * Refactor Players Search result parsing
    
    * Improve utils functions
    
    * Update unit tests

--- app/services/players/search.py
+++ app/services/players/search.py
@@ -1,9 +1,10 @@
 from dataclasses import dataclass
 from datetime import datetime
+from xml.etree import ElementTree
 
 from app.services.base import TransfermarktBase
 from app.utils.regex import REGEX_CHART_CLUB_ID
-from app.utils.utils import extract_from_url, safe_regex
+from app.utils.utils import extract_from_url, safe_regex, trim
 from app.utils.xpath import Players
 
 
@@ -38,41 +39,35 @@ class TransfermarktPlayerSearch(TransfermarktBase):
         Returns:
             list: A list of dictionaries, with each dictionary representing a player search result.
         """
-        idx = [extract_from_url(url) for url in self.get_list_by_xpath(Players.Search.URL)]
-        name = self.get_list_by_xpath(Players.Search.NAME)
-        position = self.get_list_by_xpath(Players.Search.POSITION)
-        club_name = self.get_list_by_xpath(Players.Search.CLUB_NAME)
-        club_id = [
-            safe_regex(img, REGEX_CHART_CLUB_ID, "club_id") for img in self.get_list_by_xpath(Players.Search.CLUB_IMAGE)
-        ]
-        age = self.get_list_by_xpath(Players.Search.AGE)
-        nationality = self.get_list_by_xpath(Players.Search.NATIONALITY)
-        market_value = self.get_list_by_xpath(Players.Search.MARKET_VALUE)
+        search_results: list[ElementTree] = self.page.xpath(Players.Search.RESULTS)
+        results = []
 
-        return [
-            {
-                "id": idx,
-                "name": name,
-                "position": position,
-                "club": {
-                    "id": club_id,
-                    "name": club_name,
+        for result in search_results:
+            idx = extract_from_url(result.xpath(Players.Search.ID))
+            name = trim(result.xpath(Players.Search.NAME))
+            position = trim(result.xpath(Players.Search.POSITION))
+            club_name = trim(result.xpath(Players.Search.CLUB_NAME))
+            club_id = safe_regex(result.xpath(Players.Search.CLUB_IMAGE), REGEX_CHART_CLUB_ID, "club_id")
+            age = trim(result.xpath(Players.Search.AGE))
+            nationalities = result.xpath(Players.Search.NATIONALITIES)
+            market_value = trim(result.xpath(Players.Search.MARKET_VALUE))
+
+            results.append(
+                {
+                    "id": idx,
+                    "name": name,
+                    "position": position,
+                    "club": {
+                        "name": club_name,
+                        "id": club_id,
+                    },
+                    "age": age,
+                    "nationalities": nationalities,
+                    "marketValue": market_value,
                 },
-                "age": age,
-                "nationality": nationality,
-                "marketValue": market_value,
-            }
-            for idx, name, club_id, club_name, position, age, nationality, market_value in zip(
-                idx,
-                name,
-                club_id,
-                club_name,
-                position,
-                age,
-                nationality,
-                market_value,
             )
-        ]
+
+        return results
 
     def search_players(self) -> dict:
         """
