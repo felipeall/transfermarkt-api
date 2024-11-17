73965ef981c13ae35d5f2954a12dbc5b3bd54a6b
Felipe Allegretti <felipeallh@gmail.com>
2024-11-17 19:27:06 +0100

    refactor: implement schemas in endpoints

--- app/api/endpoints/competitions.py
+++ app/api/endpoints/competitions.py
@@ -2,21 +2,22 @@ from typing import Optional
 
 from fastapi import APIRouter
 
+from app.schemas import competitions as schemas
 from app.services.competitions.clubs import TransfermarktCompetitionClubs
 from app.services.competitions.search import TransfermarktCompetitionSearch
 
 router = APIRouter()
 
 
-@router.get("/search/{competition_name}")
-def search_competitions(competition_name: str, page_number: Optional[int] = 1) -> dict:
+@router.get("/search/{competition_name}", response_model=schemas.CompetitionSearch)
+def search_competitions(competition_name: str, page_number: Optional[int] = 1):
     tfmkt = TransfermarktCompetitionSearch(query=competition_name, page_number=page_number)
     competitions = tfmkt.search_competitions()
     return competitions
 
 
-@router.get("/{competition_id}/clubs")
-def get_competition_clubs(competition_id: str, season_id: Optional[str] = None) -> dict:
+@router.get("/{competition_id}/clubs", response_model=schemas.CompetitionClubs)
+def get_competition_clubs(competition_id: str, season_id: Optional[str] = None):
     tfmkt = TransfermarktCompetitionClubs(competition_id=competition_id, season_id=season_id)
     competition_clubs = tfmkt.get_competition_clubs()
     return competition_clubs
